6122527a237be061305dabc7aede645a72f1da6b
songchao <1064883707@qq.com>
2021-09-01 12:15:26 +0800

    原生nodejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const fs = require('fs')
+const socket = require('socket.io')
 
 const server = http.createServer(function(req, res) {
     fs.readFile(__dirname + '/index.html', function(err, data) {
@@ -12,8 +13,12 @@ const server = http.createServer(function(req, res) {
     })
 }).listen(8000)
 
-const io = require('socket.io')(server)
+const io = socket(server)
 
 io.on('connection', socket => {
-    console.log('已连接')
+    // console.log('已连接')
+    socket.emit('news', { world: 'world' });
+    socket.on('哈哈', function (data) {
+        console.log(data);
+    });
 })
